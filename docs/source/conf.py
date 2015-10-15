2d574210eafc7f8fd785b2130a35ee00ac18ed72
Florian Wagner <florian.wagner@duke.edu>
2015-10-15 00:29:43 -0400

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,8 @@ import sphinx_rtd_theme
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('./scripts'))
+sys.path.insert(0, os.path.abspath('./plotting'))
 
 # -- General configuration ------------------------------------------------
 
