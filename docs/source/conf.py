0a95c97fdfe87db26d4f766d1f569bfdc6da473e
Florian Wagner <florian.wagner@duke.edu>
2015-11-10 16:45:46 -0500

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['xlmhg','sklearn','sklearn.decomposition']
+MOCK_MODULES = ['cython','xlmhg','sklearn','sklearn.decomposition']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
